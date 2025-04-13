e5666c7f56eff7b17a23ec2cca3a2560ad5d5583
Esha.Hudda <eshuhudda27@gmail.com>
2025-04-13 21:45:58 +0000

    Login to Docker Hub

--- myapp.js
+++ myapp.js
@@ -11,7 +11,7 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
 
   // Send the response body "Hello, World!"
-  res.end('Hello, World!\n');
+  res.end('Hello there, World!\n');
 });
 function add(a, b) {
     return a + b;
